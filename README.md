5826030e5dad1a77ee3198e63c546fe4788b930e
thyonox <thyonox@gmail.com>
2026-01-18 23:25:28 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# express-blog
-基于 NodeJS、Express、MongoDB 和 EJS 模板引擎构建的简单博客系统。
+# Express-Blog
+基于 NodeJS、Express、MongoDB 和 EJS 模板引擎构建的简单博客系统
+
+
+MongoDB 要设置 Network Access ，最好将本机 IP 和 代理 IP 都填上。
---
+++ server/models/Post.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const PostSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    createTime: {
+        type: Date,
+        default: Date.now
+    },
+    updateTime: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Post', PostSchema);
